d77eee86cc97a3f565e26b3a311a01f6dd2c71ba
kyewei <kyewei22@yahoo.ca>
2015-04-27 01:27:12 -0400

    Made API more restful. Now supports lookup by ID, and DELETE verbs for /id/xxxx requests, also uses async.waterful since async calls are everywhere, made routing more organized, works without database also (just doesn't store), also responds with messages showing success/fail/etc... so it has detailed messages

--- lyricScraper.js
+++ lyricScraper.js
@@ -2,9 +2,15 @@ var http = require("http");
 var request = require("request");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
+var async = require("async");
 var db = mongoose.connection;
 
-db.on("error", console.error);
+var dbConnected = false;
+db.on("error", function(err) {
+    if (!err) {
+        dbConnected = true;
+    }
+});
 db.once("open", function() {
 });
 
@@ -183,8 +189,8 @@ lyricsModeSiteOptions.getSongInfo = function ($) {
 
 
 
-function scrapSites(urlSegments) {
-    var obj = {};
+function scrapSites(urlSegments, obj) {
+    
     obj.query = {};
     if (urlSegments[2].toLowerCase()==="artist")
         obj.query.artist = urlSegments[3];
@@ -235,162 +241,338 @@ function scrapeCustomSite(urlSegments, obj, customSiteInfo) {
     // Loads that entry's URL, and scrapes if the user specified, or loads from cache otherwise
 
 
-    function lyricURLRequestCallback(err,res,html) { // callback for when the final lyric page loads
-        if (!(!err && res.statusCode === 200)) {
-            console.log("Error, loading lyric page failed",err);
-            return;
-        }
-        var $ = cheerio.load(html);
-
-        var result = customSiteInfo.getSongInfo($); // call custom site scraper
-        result.url = finalURL;
-        console.log(customSiteInfo.name+" Lyrics Ready");
+    var lastCalledFunction = null; // so err callback knows which one.
 
 
-        if (!dbobj) { // if database did not have entry, make new entry
-            dbobj = new LyricSiteQuery(result);
-            dbobj.save(function (err,dbobj) {
-                if (err) 
-                    console.log("Create Error",err,dbobj);
-            });
-        } else { // update existing document with newly fetched fields
-            dbobj.update(result,function(err,dobj) {
-                if (err) 
-                    console.log("Update Error",err,dbobj);
-            });//.exec();
-        }
-        if (!obj.query.store) {
-            dbobj.remove(function(err,dbobj){
-                if (err) {
-                    console.log("Remove Error",err,dbobj);
+    async.waterfall([
+        function sendRequest(callback) {
+            lastCalledFunction = "sendRequest";
+            request({
+                uri: searchURL,
+                method: "GET", 
+                timeout: 10000,
+                followRedirect:true,
+                maxRedirects: 10
+            }, callback);
+        },
+        function searchURLRequestCallback(res,html, callback) {
+            lastCalledFunction = "searchURLRequestCallback";
+            /*if (!(!err && res.statusCode === 200)) {
+                console.log("Error searching "+customSiteInfo.name, err);
+                return;
+            }*/
+            console.log("Searching "+ customSiteInfo.name);
+
+            var $ = cheerio.load(html);
+            var url = customSiteInfo.getLyricLink($); // call custom site scraper
+            if (!url) {
+                console.log("Error scraping link from "+customSiteInfo.name);
+                // decrement max amount of sites that can be displayed since sites found nothing.
+                ++obj.query.failed;
+            }
+
+            finalURL = url;
+            console.log("Lyric URL is:",url);
+
+
+            // tries to see if an existing query exists
+            if (dbConnected)
+                LyricSiteQuery.findOne({"url":url}, callback);
+            else
+                callback(null, null);
+        }, 
+        function mongoDBCacheLookupCallback(doc, callback) { 
+            lastCalledFunction = "mongoDBCacheLookupCallback";
+            /*if (err) {
+                console.log("Database lookup (Read) Error",err,doc);
+            }*/
+            dbobj = doc;
+            //if (dbobj) console.log(dbobj.id, dbobj._id, dbobj);
+            // reads all info from mongodb ONLY IF database has entry and user wants to use cache
+
+            if (dbobj && obj.query.usecache) { 
+                var result = {};
+
+                for (var field in LyricSiteQuery.schema.paths) {
+                    if (field.charAt(0) !== "_") {
+                        result[field] = dbobj[field];
+                    }
                 }
-            });
+                result.id = dbobj.id;
+                result.links = {};
+                result.links.href = "/id/"+result.id;
+
+                //console.log(result);
+                if (!obj.query.store) {
+                    dbobj.remove(function(err,dbobj){
+                        if (err) {
+                            console.log("Remove Error",err,dbobj);
+                        }
+                    });
+                }
+                obj.results.push(result);
+            } else { // http request to query actual lyrics site
+                request({
+                    uri: finalURL,
+                    method: "GET", 
+                    timeout: 10000,
+                    followRedirect:true,
+                    maxRedirects: 10
+                }, callback);
+            }
+        },
+        function lyricURLRequestCallback(res,html, callback) { // callback for when the final lyric page loads
+            /*if (!(!err && res.statusCode === 200)) {
+                console.log("Error, loading lyric page failed",err);
+                return;
+            }*/
+            lastCalledFunction = "lyricURLRequestCallback";
+            var $ = cheerio.load(html);
+
+            var result = customSiteInfo.getSongInfo($); // call custom site scraper
+            result.url = finalURL;
+            
+            
+            console.log(customSiteInfo.name+" Lyrics Ready");
+
+            function databaseFinishCallback() {
+                if (dbobj) {
+                    result.id = dbobj.id;
+                    result.links = {};
+                    result.links.href = "/id/"+result.id;
+                }
+                
+                obj.results.push(result);
+            }
+            if (!dbConnected) 
+                databaseFinishCallback();
+
+            if (!dbobj) { // if database did not have entry, make new entry
+                dbobj = new LyricSiteQuery(result);
+                dbobj.save(function (err,dbobj) {
+                    if (err) {
+                        console.log("Create Error",err,dbobj);
+                        return;
+                    }
+                    databaseFinishCallback();
+                });
+            } else { // update existing document with newly fetched fields
+                dbobj.update(result,function(err,dobj) {
+                    if (err) {
+                        console.log("Update Error",err,dbobj);
+                        return;
+                    }
+                    databaseFinishCallback();
+                });//.exec();
+            }
+            if (!obj.query.store) {
+                dbobj.remove(function(err,dbobj){
+                    if (err) {
+                        console.log("Remove Error",err,dbobj);
+                        return;
+                    }
+                    databaseFinishCallback();
+                });
+            }
+            
         }
-        obj.results.push(result);
+    ], function (err, arg1, arg2, arg3) {
+        if (err) {
+            switch(lastCalledFunction) {
+                case "sendRequest": 
+                    console.log("Error searching "+customSiteInfo.name, err);
+                    break;
+                case "searchURLRequestCallback": 
+                    // arg1 = doc
+                    console.log("Database lookup (Read) Error",err,arg1); 
+                    break;
+                case "mongoDBCacheLookupCallback":
+                    console.log("Error, loading lyric page failed",err);
+                    break;
+                case "lyricURLRequestCallback":
+
+                    break;
+            }
+        }
+    });
+}
+
+
+
+// ROUTING THINGS ARE BELOW
+var urlRouter = {};
+urlRouter.invalid = function(req, res) {
+    var toJson = { "request": req.url,
+                    "method": req.method,
+                    "message": "Invalid, refer to /"
+    };
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(toJson, null, 4));
+}
+urlRouter["/"] = function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello. Welcome to the lyricScraper text/JSON based API\n");
+    res.write("\n");
+    res.write("\n");
+    res.write("Use the following template for queries:\n");
+    res.write("\n");
+    res.write("/search/artist/[artist-name]/title/[song-name]/format/[filetype,default=json]/usecache/[yes|no,default=yes]/store/[yes|no,default=yes]/minimum/[number of results]\n");
+    res.write("Multiple words are separated with \+, like in \"maroon+5\"\n");
+    res.write("\n");
+    res.write("\n");
+    res.write("Go to the following link to delete the cache:\n");
+    res.write("\n");
+    res.write("/deleteall\n");
+
+    res.end();
+};
+urlRouter["/search"] = function(req, res) {
+    var url = req.url;
+    var segments = url.split("/");
+
+    var objResult = {};
+    objResult.request = url;
+    objResult.method = req.method, 
+    objResult.message = null;
+
+    if (segments.length<3) {
+        objResult.message = "Invalid, use "+"/search/artist/[artist-name]/title/[song-name]/format/[filetype,default=json]/usecache/[yes|no,default=yes]/store/[yes|no,default=yes]/minimum/[number of results]";
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(toJson, null, 4));
+        return;
+    }
+    
+    objResult.message = scrapSites(segments, objResult) ? "Found" : "Not found";
+    if (!dbConnected)
+        objResult.message += ", database not connected"
+    
+    var now = new Date();
+    function waitForAtLeastX () {
+        if (objResult.results.length<objResult.query.minimum && 
+            objResult.results.length+objResult.query.failed < objResult.query.totalsites) {
+            // less than minimum BUT ALSO having not all sites scanned
+
+            setTimeout(waitForAtLeastX,200);
+        } else {
+            if (objResult.query.format === "json") {
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(objResult, null, 4));
+            } else {
+                res.writeHead(200, {"Content-Type": "text/plain"});
+                res.write("Query to online lyric sites timed out.");
+                res.end();
+            }
+            
+        }
+    }
+    setTimeout(waitForAtLeastX,200);
+};
+urlRouter["/id"] = function(req, res) {
+    var toJson = { "request": req.url,
+                    "method": req.method
+    };
+    var segments = req.url.split("/");
+    if (segments.length<3){
+        toJson.message = "Invalid, use /id/xxxxxx";
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(toJson, null, 4));
+        return;
+    }
+    if (!dbConnected) {
+        toJson.message = "Database not connected";
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(toJson, null, 4));
+        return;
     }
 
-    function mongoDBCacheLookupCallback(err, doc) { 
-        if (err) {
-            console.log("Database lookup (Read) Error",err,doc);
-        }
-        dbobj = doc;
-        // reads all info from mongodb ONLY IF database has entry and user wants to use cache
-        if (dbobj && obj.query.usecache) { 
-            var result = {};
+    //console.log(req.method);
+    LyricSiteQuery.findById(segments[2], function searchbyIdCallback(err, dbobj) {
+        var toJson = { "request": req.url,
+                        "method": req.method
+        };
 
+        if (err || !dbobj) {
+            toJson.message = "Not found";
+
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(toJson, null, 4));
+            return;
+        }
+
+        if (req.method === "GET") {
+            var result = {};
             for (var field in LyricSiteQuery.schema.paths) {
                 if (field.charAt(0) !== "_") {
                     result[field] = dbobj[field];
                 }
             }
-            //console.log(result);
-            if (!obj.query.store) {
-                dbobj.remove(function(err,dbobj){
-                    if (err) {
-                        console.log("Remove Error",err,dbobj);
-                    }
-                });
-            }
-            obj.results.push(result);
-        } else { // http request to query actual lyrics site
-            request({
-                uri: finalURL,
-                method: "GET", 
-                timeout: 10000,
-                followRedirect:true,
-                maxRedirects: 10
-            }, lyricURLRequestCallback);
+            result.id = dbobj.id;
+            result.links = {};
+            result.links.href = "/id/"+result.id;
+            
+            toJson.results = [result];
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(toJson, null, 4));
+        } else if (req.method === "DELETE") {
+            dbobj.remove(function(err,dbobj){
+                if (err) {
+                    console.log("Remove Error",err,dbobj);
+                    return;
+                }
+                toJson.message = "Item deleted";
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(toJson, null, 4));
+            });
+        } else {
+            toJson.message = "Invalid";
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(toJson, null, 4));
         }
+
+        
+    });
+};
+urlRouter["/deleteall"] = function(req, res) {
+    if (!dbConnected) {
+        var toJson = { "request": req.url,
+                        "method": req.method,
+                        "message": "Database not connected"
+        };
+        console.log("Database not connected");
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(toJson, null, 4));
     }
+    LyricSiteQuery.remove({}, function(err) {
+        var toJson = { "request": req.url,
+                        "method": req.method,
+                        "message": "Entire cache deleted"
+        };
+        console.log("Lyrics cache deleted");
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(toJson, null, 4));
+    });
 
-    function searchURLRequestCallback(err,res,html) {
-        if (!(!err && res.statusCode === 200)) {
-            console.log("Error searching "+customSiteInfo.name, err);
-            return;
-        }
-        console.log("Searching "+ customSiteInfo.name);
-
-        var $ = cheerio.load(html);
-        var url = customSiteInfo.getLyricLink($); // call custom site scraper
-        if (!url) {
-            console.log("Error scraping link from "+customSiteInfo.name);
-            // decrement max amount of sites that can be displayed since sites found nothing.
-            ++obj.query.failed;
-        }
-
-        finalURL = url;
-        console.log("Lyric URL is:",url);
-
-
-        // tries to see if an existing query exists
-        LyricSiteQuery.findOne({"url":url}, mongoDBCacheLookupCallback);
-    }
-
-    request({
-        uri: searchURL,
-        method: "GET", 
-        timeout: 10000,
-        followRedirect:true,
-        maxRedirects: 10
-    }, searchURLRequestCallback);
-}
+};
 
 
 
+// RUN SERVER 
 function responseCallback (req, res) {
     //console.log(req);
     //res.writeHead(200);
     var url = req.url;
     console.log("Received access: "+url);
-    var segments = url.split("/");
-    //console.log(segments);
-    if (segments.length >= 2 && segments[1].toLowerCase() === "search") {
-        var objResult = scrapSites(segments);
-        var now = new Date();
-        function waitForAtLeastX () {
-            if (objResult.results.length<objResult.query.minimum && 
-                objResult.results.length+objResult.query.failed < objResult.query.totalsites) {
-                // less than minimum BUT ALSO having not all sites scanned
+    var router = url.substring(0,url.indexOf("/",1))
 
-                setTimeout(waitForAtLeastX,200);
-            } else {
-                if (objResult.query.format === "json") {
-                    res.writeHead(200, {"Content-Type": "application/json"});
-                    res.end(JSON.stringify(objResult, null, 4));
-                } else {
-                    res.writeHead(200, {"Content-Type": "text/plain"});
-                    res.write("Query to online lyric sites timed out.");
-                    res.end();
-                }
-                
-            }
-        }
-        setTimeout(waitForAtLeastX,200);
-    } else if (segments.length >=2 && segments[1].toLowerCase() === "deleteall") {
-        LyricSiteQuery.remove({}, function(err) {
-            console.log("Lyrics Cache Deleted");
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write("Lyrics Cache Deleted.\n");
-            res.end();
-        });
+    if (url === "/") {
+        setTimeout(urlRouter["/"], 2, req, res);
+    } else if (urlRouter[router]) {
+        setTimeout(urlRouter[router], 2, req, res);
     } else {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write("Hello. Welcome to the lyricScraper text/JSON based API\n");
-        res.write("\n");
-        res.write("\n");
-        res.write("Use the following template for queries:\n");
-        res.write("\n");
-        res.write("/search/artist/[artist-name]/title/[song-name]/format/[filetype,default=json]/usecache/[yes|no,default=yes]/store/[yes|no,default=yes]/minimum/[number of results]\n");
-        res.write("Multiple words are separated with \+, like in \"maroon+5\"\n");
-        res.write("\n");
-        res.write("\n");
-        res.write("Go to the following link to delete the cache:\n");
-        res.write("\n");
-        res.write("/deleteall\n");
-
-        res.end();
+        setTimeout(urlRouter.invalid, 2, req, res);
     }
 }
+
 
 var server = http.createServer(responseCallback);
 server.listen(22096);
